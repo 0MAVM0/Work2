743866bf13131bded57f94cf3dba09e5bec119e4
MVMAM <ediconpts9@gmail.com>
2024-02-27 19:44:31 +0500

    Scroll to top

--- main.js
+++ main.js
@@ -13,3 +13,26 @@ document.getElementById('submit-btn').addEventListener('click', function() {
             console.log('Письмо не удалось отправить!', error);
         });
 });
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.bottom = "40px";
+    } else {
+        scrollToTopBtn.style.bottom = "700px";
+    }
+}
+
+document.getElementById("scrollToTopBtn").onclick = function() {
+    scrollToTop();
+};
+
+function scrollToTop() {
+    var position = document.documentElement.scrollTop || document.body.scrollTop;
+    if (position) {
+        window.requestAnimationFrame(scrollToTop);
+        window.scrollTo(0, position - position / 8);
+    }
+}
